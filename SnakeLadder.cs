4c9fa7198e43a1a79150ea511b892f93f7d003fd
Ambili das v s <ambili.kan@gmail.com>
2022-05-05 22:11:19 +0530

    Checking for options in game

--- SnakeLadder.cs
+++ SnakeLadder.cs
@@ -6,18 +6,38 @@ using System.Threading.Tasks;
 
 namespace SnakeAndLadder
 {
-    internal class SnakeLadder
+    public class SnakeLadder
     {
-        int Position = 0;
-        Random random = new Random();
         
-
+        
+        public const int Noplay = 0;
+        public const int Ladder = 1;
+        public const int Snake = 2;
+        Random random = new Random();
         public void StartGame()
         {
+            int Position = 0;
             Console.WriteLine("Player starts at position {0}",Position);
             int die = random.Next(1, 7);
-            Position += die;
-            Console.WriteLine("Now at position "+Position);
+            int option=random.Next(0,3);
+
+            switch (option)
+            {
+                case Ladder:
+                    
+                   Position += die;
+                    break;
+                   
+                case Snake:
+
+                    Position -= die;
+                    break ;
+                default:
+                    Position = Position;
+                    break;
+            }
+            Console.WriteLine(Position);
+            
         }
     }
 }
