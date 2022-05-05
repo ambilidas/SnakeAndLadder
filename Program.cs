1e707118a5e5d98ab9f6c4f691a678725d33f214
Ambili das v s <ambili.kan@gmail.com>
2022-05-05 21:18:07 +0530

    Program for Snake and ladder game is created

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    public class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to snake&ladder game");
+            SnakeLadder myobj=new SnakeLadder();
+            myobj.StartGame();
+            
+        }
+    }
+}
---
+++ SnakeLadder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    internal class SnakeLadder
+    {
+        int Position = 0;
+        Random random = new Random();
+        
+
+        public void StartGame()
+        {
+            Console.WriteLine("Player starts at position {0}",Position);
+        }
+    }
+}
