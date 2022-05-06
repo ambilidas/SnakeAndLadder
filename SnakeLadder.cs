e1d7b0baadbffbe1878c7539f7fb91d46835704a
Ambili das v s <ambili.kan@gmail.com>
2022-05-06 13:08:56 +0530

    Repeat till the player reaches 100

--- SnakeLadder.cs
+++ SnakeLadder.cs
@@ -13,15 +13,58 @@ namespace SnakeAndLadder
         public const int Noplay = 0;
         public const int Ladder = 1;
         public const int Snake = 2;
-        Random random = new Random();
+       
         public void StartGame()
         {
+            Random random = new Random();
             int Position = 0;
+            int dice;
+            int option;
             Console.WriteLine("Player starts at position {0}",Position);
-            int die = random.Next(1, 7);
-            int option=random.Next(0,3);
+           
+   
 
-            switch (option)
+            while(Position < 100)
+            {
+                dice = random.Next(1, 7);
+
+                Console.WriteLine("Number appeared in dice is:" +dice);
+
+                //player option generation
+                option = random.Next(0, 3);
+                Console.WriteLine("Player option is: " +option);
+
+                if(option == Ladder)
+                {
+                    if((Position + dice) > 100)
+                    {
+                        Position = 100;
+                    }
+                    else
+                    {
+                        Position += dice;
+                    }
+                }
+
+                else if(option == Snake)
+                {
+                    if(Position < dice)
+                    {
+                        Position=0;
+                    }
+                    else
+                    {
+                        Position -= dice;
+                    }
+                }
+                else
+                {
+                    Position = Position;
+                }
+                Console.WriteLine("Player reaches at: " +Position);
+            }
+
+           /* switch (option)
             {
                 case Ladder:
                     
@@ -36,7 +79,7 @@ namespace SnakeAndLadder
                     Position = Position;
                     break;
             }
-            Console.WriteLine(Position);
+            Console.WriteLine(Position);*/
             
         }
     }
